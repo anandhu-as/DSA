689ab8c0af27ebb5e2bbb1a56e02fb1b181163d6
Anandhu as <109361830+anandhu-as@users.noreply.github.com>
2026-01-27 20:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (85.10%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+
+    let n = nums.length;
+    let actualsum = n * (n + 1) / 2;
+
+    return actualsum - sum;
+};
