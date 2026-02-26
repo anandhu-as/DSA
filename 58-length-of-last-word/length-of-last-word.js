a9d7b8579a612c3059ed59a2eeeb61eb2f681003
Anandhu as <109361830+anandhu-as@users.noreply.github.com>
2026-02-26 21:11:09 +0530

    Fix whitespace handling in lengthOfLastWord function

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,13 +5,10 @@
 var lengthOfLastWord = function(s) {
     let index = s.length - 1;
     let length = 0;
-
     while (index >= 0 && s[index] === ' ') index--;
-
     while (index >= 0 && s[index] !== ' ') {
         length++;
         index--;
     }
-
     return length;
 };
